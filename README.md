4bfdd5f0fa901b24330887f30dc9ba9f4cc479b8
Andika Chamberlin <htmlandika@gmail.com>
2019-09-27 00:28:35 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Combine Bootpack
+
+- Webpack
+- Bootstrap v3.4.1
+- Perfect Scroll
+- Lazy Load
+
+Author @andikachamberlin
+
+Powered by Flamecore Organization
---
+++ assets/js/dev.script.js
@@ -0,0 +1,16 @@
+/*------------------------------------------------------------------
+
+[Master Dev Script]
+
+Organization     : Flamecore 
+Organization URI : http://flamecore.pro
+Name Project     : 
+Version          : 1.0.0
+Author           : Andika Chamberlin
+Author URI       : http://andikachamberlin.github.io
+
+-------------------------------------------------------------------*/
+$(document).ready(function(){
+    
+
+});
